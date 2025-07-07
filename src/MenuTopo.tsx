a9fba5011df924902a6e88159d50722a9522634d
MeleysTargaryen <raysa.santos@estudante.ifms.edu.br>
2025-07-07 16:42:54 -0400

    #Criação de componente

---
+++ src/MenuTopo.tsx
@@ -0,0 +1,14 @@
+import './Produtos.css';
+
+export default function MenuTopo() {
+  return (
+    <nav className="menu-topo">
+      <ul>
+        <li><a href="#">Acessórios</a></li>
+        <li><a href="#">Pelúcias</a></li>
+        <li><a href="#">Produtos Gerais</a></li>
+        <li><a href="#">Ofertas</a></li>
+      </ul>
+    </nav>
+  );
+}
