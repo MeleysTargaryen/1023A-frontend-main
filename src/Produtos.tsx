016efcbe499d1048a63a222cd13d0e177247e22f
MeleysTargaryen <raysa.santos@estudante.ifms.edu.br>
2025-07-08 10:13:52 -0400

    #Adicionei mais produtos

--- src/Produtos.tsx
+++ src/Produtos.tsx
@@ -58,11 +58,53 @@ export default function Produtos() {
                 <p className="preco">R$150</p>
               </div>
             </div>
-
+            <div className="card-produto">
+              <img
+                src="https://img.ltwebstatic.com/images3_spmp/2024/06/07/31/1717734554a3d17fd0e6d1735dfb3a0a801cafec47_thumbnail_384x.webp"
+                alt="Pulseira "
+              />
+              <div className="info">
+                <h2>Bracelete pin</h2>
+                <p className="descricao">Bracelete com pingentes</p>
+                <p className="preco">R$20</p>
+              </div>
+            </div>
+            <div className="card-produto">
+              <img
+                src="https://img.ltwebstatic.com/images3_spmp/2024/06/14/96/17183676360b184f9c2b04f19b14d3e06c2d743f42_thumbnail_405x.webp"
+                alt="Acessorio"
+              />
+              <div className="info">
+                <h2>Gargantilha Marfin</h2>
+                <p className="descricao">Gargantilha com berlocs com o tema fundo do mar</p>
+                <p className="preco">R$50</p>
+              </div>
+            </div>
+            <div className="card-produto">
+              <img
+                src="https://http2.mlstatic.com/D_NQ_NP_637239-MLU73206981987_122023-O.webp"
+                alt="Pelucia"
+              />
+              <div className="info">
+                <h2>Pelúcia Tartaruga</h2>
+                <p className="descricao">Pelucia fofa tarataruga 50% algodão 80cm</p>
+                <p className="preco">R$250</p>
+              </div>
+            </div>
+            <div className="card-produto">
+              <img
+                src="https://img.ltwebstatic.com/v4/j/spmp/2025/05/26/63/17482635931cd4cc13006c12e5497012cabbb6b0f1_wk_1748347590_thumbnail_560x.webp"
+                alt="Vestuario"
+              />
+              <div className="info">
+                <h2>Camiseta </h2>
+                <p className="descricao">Camiseta fofa  100% algodão </p>
+                <p className="preco">R$50</p>
+              </div>
+            </div>
           </div>
         </main>
       </div>
     </>
   );
 }
-
